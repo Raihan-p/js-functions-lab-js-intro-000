2470951345f5456847218e7da181e1937cc4f67b
Raihan Preena <raihanpreena@gmail.com>
2021-03-01 03:14:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // write your code below!
 function happyHolidays() {
-return "Happy holidays!";
+return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
-console.log(name)
-return `Happy holidays, ${name!}`;
+return `Happy holidays, ${name}!`
 }
 
 function happyCustomHolidaysto
